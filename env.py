a970af1e70fd70ee8d26d18586ac6e02c149a6f5
jakharamit44 <125469531+jakharamit44@users.noreply.github.com>
2023-10-27 12:01:30 +0530

    Update env.py

--- env.py
+++ env.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.getenv("API_ID", "").strip()
-API_HASH = os.getenv("API_HASH", "").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
-DATABASE_URL = os.getenv("DATABASE_URL", "").strip() # Not a necessary variable anymore but you can add to get stats
-MUST_JOIN = os.getenv("MUST_JOIN", "")
+API_ID = os.getenv("API_ID", "17737898").strip()
+API_HASH = os.getenv("API_HASH", "ad762fe0516f367115ba651d929cf429").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "5262241773:AAGrMSq1EFsnejpMij3jLsUI5n4ut2UnGOs").strip()
+DATABASE_URL = os.getenv("DATABASE_URL", "postgres://ikoczxuu:3bXZj-H3p8phWASpj4ADkkNxt9JPxbHz@rain.db.elephantsql.com/ikoczxuu").strip() # Not a necessary variable anymore but you can add to get stats
+MUST_JOIN = os.getenv("MUST_JOIN", "toskings")
 
 if not API_ID:
     raise SystemExit("No API_ID found. Exiting...")
